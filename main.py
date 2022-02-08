ec3a0b12fc6241ecbbab7ab4bd2c67cf58d26c15
UlisesJE <ujuareze1700@alumno.ipn.mx>
2022-02-08 16:50:54 -0600

    redireccion pag inicio

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app.secret_key="jcp_helper"
 @app.route('/')
 def index():
     
-    return render_template('index.html')
+    return render_template('bienvenida.html')
 
 if __name__=='__main__':
     app.run(debug=True)
