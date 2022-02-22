b65d754dde213990c050f10758004da99507f547
luis <luisechvz@gmail.com>
2022-02-21 23:31:16 -0600

    Cambios en las imagenes, correcion de carouseles e inicio de seccion de stats

--- static/js/carouselCodeMine.js
+++ static/js/carouselCodeMine.js
@@ -1,6 +1,57 @@
-$('.owl-carousel').owlCarousel({
+/*
+    Este es el carousel para las opciones principales.
+*/
+
+$('.owl-one').owlCarousel({
     loop:false,
     margin:20,
+    autoWidth:true,
+    touchDrag:true,
+    nav:false,
+    dotsData: false,
+    dots: false,
+    fluidSpeed: true,
+    center: true,
+    responsive:{
+        0:{
+            items:1.5
+        },
+        600:{
+            items:1.5
+        }
+    }
+})
+
+
+/*
+    Es el carousel para nuestra parte de los informes
+*/
+$('.owl-two').owlCarousel({
+    loop:false,
+    margin:10,
+    autoWidth:true,
+    touchDrag:true,
+    nav:false,
+    dotsData: false,
+    dots: false,
+    fluidSpeed: true,
+    center: true,
+    responsive:{
+        0:{
+            items:1.5
+        },
+        600:{
+            items:1.5
+        }
+    }
+})
+
+/*
+    Es el carousel para nuestra parte las estadisticas del sistema
+*/
+$('.owl-three').owlCarousel({
+    loop:false,
+    margin:10,
     autoWidth:true,
     touchDrag:true,
     nav:false,
