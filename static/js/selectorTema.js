9dea49b1a4b264a71204279e3c6bfdd590bc6a4e
luis <luisechvz@gmail.com>
2022-06-02 23:30:40 -0500

    nuevos cambios en los temas

--- static/js/selectorTema.js
+++ static/js/selectorTema.js
@@ -20,6 +20,9 @@ $(document).ready(function () {
     automaticSetter();
 
     //Definir los temas nuevos en el localStorage
+    /*
+        Temas claros
+    */
     function normal(){
         localStorage.setItem("tema", "../static/css/temas/coloresDefault.css"); 
         temaGuardado = localStorage.getItem("tema");
@@ -50,30 +53,81 @@ $(document).ready(function () {
         temaGuardado = localStorage.getItem("tema");
     }
 
+    
+    function coloresNaranja(){
+        localStorage.setItem("tema", "../static/css/temas/coloresNaranja.css"); 
+        temaGuardado = localStorage.getItem("tema");
+    }
+    
+    function coloresPink(){
+        localStorage.setItem("tema", "../static/css/temas/coloresPink.css"); 
+        temaGuardado = localStorage.getItem("tema");
+    }
+    
+    function coloresPurple(){
+        localStorage.setItem("tema", "../static/css/temas/coloresPurple.css"); 
+        temaGuardado = localStorage.getItem("tema");
+    }
+    
+    function coloresRed(){
+        localStorage.setItem("tema", "../static/css/temas/coloresRed.css"); 
+        temaGuardado = localStorage.getItem("tema");
+    }
+    
+    /*
+    Temas claros
+    */
+   
     function coloresModoOscuro(){
         localStorage.setItem("tema", "../static/css/temas/coloresModoOscuro.css"); 
         temaGuardado = localStorage.getItem("tema");
     }
 
-    function coloresNaranja(){
-        localStorage.setItem("tema", "../static/css/temas/coloresNaranja.css"); 
+    function dark_aqua(){
+        localStorage.setItem("tema", "../static/css/temas/dark_aqua.css"); 
         temaGuardado = localStorage.getItem("tema");
     }
 
-    function coloresPink(){
-        localStorage.setItem("tema", "../static/css/temas/coloresPink.css"); 
+    function dark_blue(){
+        localStorage.setItem("tema", "../static/css/temas/dark_blue.css"); 
         temaGuardado = localStorage.getItem("tema");
     }
 
-    function coloresPurple(){
-        localStorage.setItem("tema", "../static/css/temas/coloresPurple.css"); 
+    function dark_green(){
+        localStorage.setItem("tema", "../static/css/temas/dark_green.css"); 
         temaGuardado = localStorage.getItem("tema");
     }
 
-    function coloresRed(){
-        localStorage.setItem("tema", "../static/css/temas/coloresRed.css"); 
+    function dark_orange(){
+        localStorage.setItem("tema", "../static/css/temas/dark_orange.css"); 
         temaGuardado = localStorage.getItem("tema");
     }
+
+    function dark_pink(){
+        localStorage.setItem("tema", "../static/css/temas/dark_pink.css"); 
+        temaGuardado = localStorage.getItem("tema");
+    }
+
+    function dark_pink(){
+        localStorage.setItem("tema", "../static/css/temas/dark_pink.css"); 
+        temaGuardado = localStorage.getItem("tema");
+    }
+
+    function dark_purple(){
+        localStorage.setItem("tema", "../static/css/temas/dark_purple.css"); 
+        temaGuardado = localStorage.getItem("tema");
+    }
+
+    function dark_red(){
+        localStorage.setItem("tema", "../static/css/temas/dark_red.css"); 
+        temaGuardado = localStorage.getItem("tema");
+    }
+
+    function dark_yellow(){
+        localStorage.setItem("tema", "../static/css/temas/dark_yellow.css"); 
+        temaGuardado = localStorage.getItem("tema");
+    }
+    
 
     //Funciones de click en cada uno de los temas
     document.getElementById('coloresDefault').addEventListener('click', function () {
@@ -128,6 +182,46 @@ $(document).ready(function () {
 
     document.getElementById('coloresRed').addEventListener('click', function () {
         coloresRed();
+        tema.attr("href", temaGuardado);
+    })
+
+    document.getElementById('dark_aqua').addEventListener('click', function () {
+        dark_aqua();
+        tema.attr("href", temaGuardado);
+    })
+
+    document.getElementById('dark_blue').addEventListener('click', function () {
+        dark_blue();
+        tema.attr("href", temaGuardado);
+    })
+
+    document.getElementById('dark_green').addEventListener('click', function () {
+        dark_green();
+        tema.attr("href", temaGuardado);
+    })
+    
+    document.getElementById('dark_orange').addEventListener('click', function () {
+        dark_orange();
+        tema.attr("href", temaGuardado);
+    })
+
+    document.getElementById('dark_pink').addEventListener('click', function () {
+        dark_pink();
+        tema.attr("href", temaGuardado);
+    })
+
+    document.getElementById('dark_purple').addEventListener('click', function () {
+        dark_purple();
+        tema.attr("href", temaGuardado);
+    })
+
+    document.getElementById('dark_red').addEventListener('click', function () {
+        dark_red();
+        tema.attr("href", temaGuardado);
+    })
+    
+    document.getElementById('dark_yellow').addEventListener('click', function () {
+        dark_yellow();
         tema.attr("href", temaGuardado);
     })
 
