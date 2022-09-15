12b8df1a1e66f20ff65a5c5673e62a378a0d2ffb
luis <luisechvz@gmail.com>
2022-09-15 15:07:39 -0500

    gestión de grupos ajustes interfaz

--- static/js/bienvenidaEfectos.js
+++ static/js/bienvenidaEfectos.js
@@ -1,9 +1,13 @@
 const fondoBienvenida = document.querySelector('#fondoBienvenida');
 const fondoActual = localStorage.getItem('fondoElegidoPorUsuario');
 
-//Esta validación es para que en caso de que exista el
-//fondo guardado en localStorage lo recuerde
-//sino, lo que hace es poner el fondo por defecto.
-if (localStorage.getItem('fondoElegidoPorUsuario') != undefined) {
-    fondoBienvenida.style.backgroundImage = "url('" + fondoActual + "')";
+function aplicarFondoSidenav(){
+    //Esta validación es para que en caso de que exista el
+    //fondo guardado en localStorage lo recuerde
+    //sino, lo que hace es poner el fondo por defecto.
+    if (localStorage.getItem('fondoElegidoPorUsuario') != undefined) {
+        fondoBienvenida.style.backgroundImage = "url('" + fondoActual + "')";
+    }
 }
+
+aplicarFondoSidenav();
