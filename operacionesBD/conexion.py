62a475eefa40b605b12add4eeeade78cb3e17d24
luis <luisechvz@gmail.com>
2022-09-25 19:33:27 -0500

    Prueba rapida base de datos heroku

--- operacionesBD/conexion.py
+++ operacionesBD/conexion.py
@@ -1,4 +1,6 @@
 import pymysql
 
 def obtener_conexion():
-    return pymysql.connect(host='localhost',user='root',password='',db='jcp_helper_db')
+    #Conexion para heroku
+    return pymysql.connect(host='qvti2nukhfiig51b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',user='etiaob16vzbuo4cu',password='n8zzmlh5wda1ihbk',db='l5l3jax1eg8p2sg0')
+    #return pymysql.connect(host='localhost',user='root',password='',db='jcp_helper_db')
