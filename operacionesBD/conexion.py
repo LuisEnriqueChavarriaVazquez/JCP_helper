c010f1f86ddda9725177eeb8f5accd82bbc529c7
luis <luisechvz@gmail.com>
2022-09-25 19:47:59 -0500

    Restablecimiento de la DB

--- operacionesBD/conexion.py
+++ operacionesBD/conexion.py
@@ -2,5 +2,5 @@ import pymysql
 
 def obtener_conexion():
     #Conexion para heroku
-    return pymysql.connect(host='qvti2nukhfiig51b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',user='etiaob16vzbuo4cu',password='n8zzmlh5wda1ihbk',db='l5l3jax1eg8p2sg0')
-    #return pymysql.connect(host='localhost',user='root',password='',db='jcp_helper_db')
+    #return pymysql.connect(host='qvti2nukhfiig51b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',user='etiaob16vzbuo4cu',password='n8zzmlh5wda1ihbk',db='l5l3jax1eg8p2sg0')
+    return pymysql.connect(host='localhost',user='root',password='',db='jcp_helper_db')
