e58ab11c09397810ec2e4669f2d2a0354de51390
luis <luisechvz@gmail.com>
2022-10-12 12:26:12 -0500

    Opcion multiple visualizable

--- static/js/operacionesVerCuestionario.js
+++ static/js/operacionesVerCuestionario.js
@@ -32,51 +32,87 @@ function obtenerDataCuestionario() {
     }
 
     //Insertamos cajas de acuerdo a la cantidad de preguntas
-    function insertarCajasPreguntas() {
-        let cantidadPreguntas = contarPreguntas();
+    function insertarCajasPreguntas(numeroPregunta, tipo) {
         const contenedorPadre = document.getElementById('contenedorPreguntasPreview');
         let contenedor;
 
-        for (var t = 0; t < cantidadPreguntas; t++) {
-            contenedor = `<div class="bordered2 colorGreyWhiter shadow-1e preguntaContainerIndividualPreview"></div>`;
+        for (var t = 0; t < numeroPregunta; t++) {
+            contenedor = `<div class="` + tipo + ` bordered2 colorGreyWhiter shadow-1e preguntaContainerIndividualPreview"></div>`;
             contenedorPadre.innerHTML += contenedor;
         }
     }
-    insertarCajasPreguntas();
 
-    //Preguntas ingresadas dependiendo del tipo
-    function ingresarPreguntasEjercicios() {
+    //Preguntas de opcion multiple
+    function ingresarPreguntasOpcionMultiple(){
+        //Ingresamos los elementos
+        insertarCajasPreguntas(preguntasModalArray1.length, 'opt1');
         //Accedemos a las cajas
-        let contenedoresPregunta = document.getElementsByClassName('preguntaContainerIndividualPreview');
+        let contenedoresPregunta = document.getElementsByClassName('opt1');
+        //Hacemos variables de pregunta
+        let dato1_pregunta, dato2_media, dato3_respuestaCorrecta, dato4_opt1, dato5_opt2, dato6_opt3, dato7_opt4;
+
+        for (var m = 0; m < preguntasModalArray1.length; m++) {
+            //Contenido de la pregunta
+            let contenido =
+            `  
+                <h5><b>Ejercicio opcion multiple ` + (m + 1) + `</b></h5>
+                <hr>
+                <div class="preguntaBox colorGrey bordered1"><b>` + preguntasModalArray1[m][0] + `</b></div>
+                <div class="imagenBox bordered1 shadow-1e">
+                    <img class="bordered1" src="` + preguntasModalArray1[m][1] + `"></img>
+                </div>
+                <div class="contendorRecursoOnlineBox"> 
+                    <a href="` + preguntasModalArray1[m][2] + `" class="waves-effect waves-light btn color2 bordered5 btnPreguntaStyleFormat"><i class="material-icons left">cloud</i>Acceder a recurso online</a>
+                </div>
+                <h6><b>Opciones de la pregunta.</b></h6>
+                <div class="opcionesContainerStyleViewCuestionario">
+                    <div class="color5 bordered1 coloredText letterStyleViewCuestionario">A</div><div class="colorGrey bordered1 opcionStyleViewCuestionario">` + preguntasModalArray1[m][3] + `</div>
+                    <div class="color5 bordered1 coloredText letterStyleViewCuestionario">B</div><div class="colorGrey bordered1 opcionStyleViewCuestionario">` + preguntasModalArray1[m][4] + `</div>
+                    <div class="color5 bordered1 coloredText letterStyleViewCuestionario">C</div><div class="colorGrey bordered1 opcionStyleViewCuestionario">` + preguntasModalArray1[m][5] + `</div>
+                    <div class="color5 bordered1 coloredText letterStyleViewCuestionario">D</div><div class="colorGrey bordered1 opcionStyleViewCuestionario">` + preguntasModalArray1[m][6] + `</div>
+                </div>
+                <div class="preguntaBox color4 bordered1"><b>Respuesta correcta: ` + preguntasModalArray1[m][2] + `</b></div>
+            `;
+
+            contenedoresPregunta[m].innerHTML = contenido;
+
+        }
+        
+    }
+    
+    function ingresarPreguntasEjercicios() {
+        //Ingresamos los elementos
+        insertarCajasPreguntas(preguntasModalArray3.length, 'opt3');
+        //Accedemos a las cajas
+        let contenedoresPregunta = document.getElementsByClassName('opt3');
         //Hacemos variables de pregunta
         let dato1_ejercicio, dato2_media, dato3_carpetaOnline, dato4_codigo, dato5_output;
-
+        
         for (var k = 0; k < preguntasModalArray3.length; k++) {
             //Contenido de la pregunta
             let contenido =
             `  
-                <h5><b>Ejercicio ` + (k + 1) + `</b></h5>
-                <hr>
-                <div class="preguntaBox colorGrey bordered1"><b>` + preguntasModalArray3[k][0] + `</b></div>
-                <div class="imagenBox bordered1 shadow-1e">
-                    <img class="bordered1" src="` + preguntasModalArray3[k][1] + `"></img>
-                </div>
-                <div class="contendorRecursoOnlineBox"> 
-                    <a href="` + preguntasModalArray3[k][2] + `" class="waves-effect waves-light btn color2 bordered5 btnPreguntaStyleFormat"><i class="material-icons left">cloud</i>Acceder a recurso online</a>
-                </div>
-                <h6><b>Preview de propuesta de código.</b></h6>
-                <div class="codeContainerBox"><pre class="previewCodeContainer colorText bordered1">` + preguntasModalArray3[k][3] + `</pre></div>
-                <div class="preguntaBox color5 bordered1"><b>Output esperado: ` + preguntasModalArray3[k][4] + `</b></div>
+            <h5><b>Ejercicio ` + (k + 1) + `</b></h5>
+            <hr>
+            <div class="preguntaBox colorGrey bordered1"><b>` + preguntasModalArray3[k][0] + `</b></div>
+            <div class="imagenBox bordered1 shadow-1e">
+            <img class="bordered1" src="` + preguntasModalArray3[k][1] + `"></img>
+            </div>
+            <div class="contendorRecursoOnlineBox"> 
+            <a href="` + preguntasModalArray3[k][2] + `" class="waves-effect waves-light btn color2 bordered5 btnPreguntaStyleFormat"><i class="material-icons left">cloud</i>Acceder a recurso online</a>
+            </div>
+            <h6><b>Preview de propuesta de código.</b></h6>
+            <div class="codeContainerBox"><pre class="previewCodeContainer colorText bordered1">` + preguntasModalArray3[k][3] + `</pre></div>
+            <div class="preguntaBox color4 bordered1"><b>Output esperado: ` + preguntasModalArray3[k][4] + `</b></div>
             `;
-
+            
             contenedoresPregunta[k].innerHTML = contenido;
-        }
-
-
+        }        
     }
 
+    ingresarPreguntasOpcionMultiple();
     ingresarPreguntasEjercicios();
-
+    
     //Imprimimos la data obtenida
     //console.log(typeof dataCuestionarioJSON);
     //console.log(dataCuestionarioJSON);
