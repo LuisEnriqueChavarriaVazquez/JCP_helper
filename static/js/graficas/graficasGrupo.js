93c373c24bb593a80b04d2b1bf7bbb8cf07b11f7
luis <luisechvz@gmail.com>
2022-11-03 10:18:42 -0600

    Primera gráfica de promedios entre grupos terminada

--- static/js/graficas/graficasGrupo.js
+++ static/js/graficas/graficasGrupo.js
@@ -3,53 +3,127 @@
 <input type="hidden" value="{{datosCuestionariosProfe}}" id="datosCuestionariosGeneralesCuestionarios">
 <input type="hidden" value="{{datosCuestionariosTerminados}}" id="cuestionariosTerminados"></input> */
 
+////////////////////////////////////////////////////////
 //Accedemos a la data como string
 let datosCuestionariosTerminados = document.getElementById('cuestionariosTerminados').value;
-//Como en python eran tuplas, necesitamos convertir todo en array
-datosCuestionariosTerminados = datosCuestionariosTerminados.replaceAll('(', '[');
-datosCuestionariosTerminados = datosCuestionariosTerminados.replaceAll(')', ']');
-datosCuestionariosTerminados = datosCuestionariosTerminados.replaceAll("\'","\"");
-datosCuestionariosTerminados = datosCuestionariosTerminados.replaceAll("None", "0");
-//Son los datos de los cuestionarios evaluados. (en estado ready)
-datosCuestionariosTerminados = JSON.parse(datosCuestionariosTerminados);
-console.log('datosCuestionariosTerminados', datosCuestionariosTerminados); 
+datosCuestionariosTerminados = limpiarDatos(datosCuestionariosTerminados);
+//console.log('Cuestionarios terminados = ', datosCuestionariosTerminados);
 
+////////////////////////////////////////////////////////
+//Accedemos a los Promedios de los cuestionarios
+let promediosCuestionariosHechos = datosCuestionariosTerminados.map((element) => {
+    return element[6];
+});
+//console.log('Promedios de los cuestionarios hechos = ', promediosCuestionariosHechos)
+
+////////////////////////////////////////////////////////
 //Accedemos a los IDS de los cuestionarios
-let idsGrupos = datosCuestionariosTerminados.map((element) => {
+let idsCuestionariosHechos = datosCuestionariosTerminados.map((element) => {
     return element[1];
 });
+//console.log('Ids de los cuestionarios hechos = ', idsCuestionariosHechos)
 
-//Accedemos a los nombres de los grupos a los que pertenecen los cuestionarios
-let datosGeneralesCuestionarios = document.getElementById('datosCuestionariosGeneralesCuestionarios').value;
-//Como en python eran tuplas, necesitamos convertir todo en array
-datosGeneralesCuestionarios = datosGeneralesCuestionarios.replaceAll('(', '[');
-datosGeneralesCuestionarios = datosGeneralesCuestionarios.replaceAll(')', ']');
-datosGeneralesCuestionarios = datosGeneralesCuestionarios.replaceAll("\'","\"");
-datosGeneralesCuestionarios = datosGeneralesCuestionarios.replaceAll("None", "0");
-//Son los datos de los cuestionarios evaluados. (en estado ready)
-datosGeneralesCuestionarios = JSON.parse(datosGeneralesCuestionarios);
-console.log(datosGeneralesCuestionarios);
-let nombreGrupos
+////////////////////////////////////////////////////////
+//Accedemos a los nombres de los grupos del docente
+let datosCuestionariosProfe = document.getElementById('datosCuestionariosGeneralesCuestionarios').value;
+datosCuestionariosProfe = limpiarDatos(datosCuestionariosProfe);
+//console.log('Datos de los cuestionarios = ',datosCuestionariosProfe);
+
+////////////////////////////////////////////////////////
+//Extraemos los IDs de los grupos
+let idGrupos = [];
+idsCuestionariosHechos.map((idGrupo) => {
+    datosCuestionariosProfe.forEach(grupoId => {
+        if(idGrupo == grupoId[0]){      
+            idGrupos.push(grupoId[1]);
+        }
+    });
+});
+//console.log('idGrupos', idGrupos)
+
+
+////////////////////////////////////////////////////////
+//Accedemos a los nombres de los grupos del docente
+let datosGrupo = document.getElementById('datosDeLosGrupos').value;
+datosGrupo = limpiarDatos(datosGrupo);
+//console.log('Datos de los grupos = ', datosGrupo);
+
+////////////////////////////////////////////////////////
+//Lo definimos como set para eliminar los repetidos
+let gruposNames = new Set ();
+let gruposIDSet = new Set ();
+datosGrupo.filter((grupo) =>{
+    idGrupos.forEach(id => {
+        if(id == grupo[0]){
+            gruposNames.add(grupo[2]);
+            gruposIDSet.add(id);
+        }
+    });
+});
+let gruposNameArray = Array.from(gruposNames);
+let gruposIdsArray = Array.from(gruposIDSet);
+//console.log('gruposNameArray', gruposNameArray);
+//console.log('gruposIDSet', gruposIdsArray)
+
+/////////////////////////////////////////////////////////
+//Debemos vincular los promedios con su respectivo grupo
+/*  
+*   promediosCuestionariosHechos
+*   idGrupos  
+*/
+//Juntamos en un array de cadenas idgrupo_promedio
+let counter = 0;
+let sumaPromedioGrupos = idGrupos.map(id => {
+    return `${id}_${promediosCuestionariosHechos[counter++]}`;
+});
+//console.log('sumaPromedioGrupos', sumaPromedioGrupos);
+
+//Contamos la cantidad de elemento pertenecientes a un grupo
+let contadorPosicionesIds = [];
+for(var i = 0; i < gruposIdsArray.length; i++){
+    contadorPosicionesIds.push(idGrupos.filter(x => x === gruposIdsArray[i]).length);
+}
+//console.log('contadorPosicionesIds', contadorPosicionesIds)
+
+//Debemos sumar los puntajes de cada grupo
+let puntajePorGrupo;
+let puntajePorGrupoArray = [];
+for(var j = 0; j < contadorPosicionesIds.length; j++){
+    puntajePorGrupo = 0;
+    for(var r = 0; r < contadorPosicionesIds[j]; r++){
+        puntajePorGrupo += parseFloat(promediosCuestionariosHechos[r])
+    }
+    r = 0;
+    puntajePorGrupoArray.push(puntajePorGrupo);
+    for(var h = 0; h < contadorPosicionesIds[j]; h++){
+        promediosCuestionariosHechos.shift();
+    }
+}
+//console.log('Puntaje por grupo', puntajePorGrupoArray);
+
+//Debemos dividir los puntajes entre el total de elementos por grupo
+contador = 0;
+let promedioFinalPorGrupo = puntajePorGrupoArray.map(sumatoria => {
+    return sumatoria / contadorPosicionesIds[contador++];
+});
+//console.log('Promedio por cada grupo', promedioFinalPorGrupo);
 
 
 
-//Definición de la gráfica por default
-var comillas_simples = new RegExp("\'", "g"); //Surve para el formato de la gráfica
+let promediosPorGrupo = {}; //Objeto
+idGrupos.forEach(id => {
+    promediosPorGrupo[id] = "2332";
+})
 
-let xgrupos = $('#xgrupos').val() //Accedemos al valor del input
-xgrupos = xgrupos.replace(comillas_simples, "\"") //Damos formato al texto
-xgrupos = JSON.parse(xgrupos) //Convertimos a formato de objeto
-console.log('xgrupos', xgrupos)
+//console.log('promediosPorGrupo', promediosPorGrupo)
 
-let ygrupos = $('#ygrupos').val()
-ygrupos = ygrupos.replace(comillas_simples, "\"")
-ygrupos = JSON.parse(ygrupos)
-console.log('ygrupos', ygrupos)
 
+////////////////////////////////////////////////////////
+//Impresión de las gráficas.
 var trace1 = {
     type: 'bar', //Definimos el tipo de gráfico
-    x: xgrupos, //Pasamos la data para X
-    y: ygrupos, //Pasamos la data para y
+    x: gruposNameArray, //Nombre de los grupos
+    y: promedioFinalPorGrupo, //Pasamos la data para y
     marker: { //Definimos los estilos
         color: ['#628E90', '#256D85', '#7895B2', '#5F6F94', '#2B4865'],
         line: {
@@ -83,17 +157,17 @@ function grafica_barras() {
             }
         }
     };
-
+    
     var data = [trace1];
-
+    
     var config = { responsive: true }
-
+    
     Plotly.newPlot('graph1', data, layout, config);
 }
 
 //Imprime la gráfica pastel
 function grafica_pastel() {
-
+    
     var data2 = [{
         values: [19, 26, 55],
         labels: ['Residential', 'Non-Residential', 'Utility'],
@@ -104,7 +178,7 @@ function grafica_pastel() {
     }];
 
     var config = { responsive: true }
-
+    
     Plotly.newPlot('graph1', data2, layout, config);
 }
 
@@ -115,16 +189,28 @@ function grafica_lineal() {
         y: [10, 15, 13, 17],
         type: 'scatter'
     };
-
+    
     var trace2 = {
         x: [1, 2, 3, 4],
         y: [16, 5, 11, 9],
         type: 'scatter'
     };
-
+    
     var data = [trace1, trace2];
-
+    
     var config = { responsive: true }
-
+    
     Plotly.newPlot('graph1', data, layout, config);
+}
+
+//Funcion para limpiar los datos
+function limpiarDatos(string){
+    //Como en python eran tuplas, necesitamos convertir todo en array
+    string = string.replaceAll('(', '[');
+    string = string.replaceAll(')', ']');
+    string = string.replaceAll("\'","\"");
+    string = string.replaceAll("None", "0");
+    //Son los datos de los cuestionarios evaluados. (en estado ready)
+    string = JSON.parse(string);
+    return string
 }
