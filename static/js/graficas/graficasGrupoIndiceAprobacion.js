fb39b20edc8a6f0badb6547bd36ed3b8e9c34140
luis <luisechvz@gmail.com>
2022-11-04 14:13:41 -0600

    Estilos de análisis de indice de reprobacion

--- static/js/graficas/graficasGrupoIndiceAprobacion.js
+++ static/js/graficas/graficasGrupoIndiceAprobacion.js
@@ -12,7 +12,7 @@ function grafica_barras_indice_aprobacion() {
             x: gruposNameArray,
             y: aprobacionTotal[contador++], //listo
             name: claseAlumno,
-            type: 'bar'
+            type: 'bar',
         }
     });
 
@@ -58,4 +58,33 @@ function grafica_pastel_indice_aprobacion() {
     var config = { responsive: true } //Ajuste responsivo
 
     Plotly.newPlot('graph2', data2, layout, config);
+}
+
+function barras_porcentage_por_grupo_aprobacion(){
+    //Vaciamos de manera manual contenido de la caja
+    let container = document.getElementById('graph2');
+    container.innerHTML = "";
+
+    container.innerHTML = `
+        <section class="contenedorAnalisisPorcentage">
+            <section class="contenedorAnalisisPorcentage_son1 bordered1 colorGreyWhiter shadow-1e">
+                <p class="dato_son1">
+                    <span class="dato_1_son1">2</span> / <span class="dato_2_son1">10</span> aprueban.
+                </p>
+            </section>
+            <section class="contenedorAnalisisPorcentage_son2">
+                <div class="significado_dato_son2">
+                    <div class="color2 simbologiaBarra bordered1"> </div>
+                    <div class="colorTextReverse textoSignificado">Aprobado</div>
+                </div>
+                <div class="significado_dato_son2">
+                    <div class="color3 simbologiaBarra bordered1"> </div>
+                    <div class="colorTextReverse textoSignificado">Reprobado</div>
+                </div>
+            </section>
+            <section class="contenedorAnalisisPorcentage_son3>
+
+            </section>
+        </section>
+    `;
 }
