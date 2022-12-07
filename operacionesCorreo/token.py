7539fd23f4c22535e54fe0b2ca38877f4aced333
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2022-12-06 23:22:03 -0600

    complemento token correo

--- operacionesCorreo/token.py
+++ operacionesCorreo/token.py
@@ -1,1 +1,26 @@
 from itsdangerous import URLSafeTimedSerializer
+
+secret_key_provisional= "secret_key"
+security_password_salt = "my_precious_two"
+
+def generate_confirmation_token(email):
+    #serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    serializer = URLSafeTimedSerializer(secret_key_provisional)
+    #return serializer.dumps(email, salt=app.config['SECURITY_PASSWORD_SALT'])
+    return serializer.dumps(email, salt=security_password_salt)
+
+
+
+def confirm_token(token, expiration=3600):
+    #serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    serializer = URLSafeTimedSerializer(secret_key_provisional)
+    try:
+        email = serializer.loads(
+            token,
+            #salt=app.config['SECURITY_PASSWORD_SALT'],
+            salt = security_password_salt ,
+            max_age=expiration
+        )
+    except:
+        return False
+    return email
