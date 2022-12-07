1e59f447c61386f025f4cbefd411d581fcc18887
UlisesJE <ujuareze1700@alumno.ipn.mx>
2022-12-07 16:56:38 -0600

    mostrar un mensaje de no coincidencias en la busqueda

--- routes/profesor.py
+++ routes/profesor.py
@@ -864,7 +864,7 @@ def cuestionarios_like_titulo():
         cuestionarios_generales = ast.literal_eval(cuestionarios_generales)
         return render_template('profesor/a_comunidad_profesor.html',general=cuestionarios_generales,python=cuestionarios_python(),lenguajec=cuestionarios_c(),java=cuestionarios_java())
     except:
-        flash('No se han encontrado cuestionarios con un titulo coincidente con ese criterio', 'danger')
+        flash('No se han encontrado cuestionarios coincidentes', 'danger')
         return render_template('profesor/a_comunidad_profesor.html',python=cuestionarios_python(),lenguajec=cuestionarios_c(),java=cuestionarios_java())
 
 
