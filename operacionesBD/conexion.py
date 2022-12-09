936e491f27bebcfb018e93a0e173a50841cd9c54
UlisesJE <ujuareze1700@alumno.ipn.mx>
2022-12-08 20:55:03 -0600

    primer intento conexion mysql railway

--- operacionesBD/conexion.py
+++ operacionesBD/conexion.py
@@ -1,6 +1,8 @@
 import pymysql
+import os
 
 def obtener_conexion():
-    #Conexion para heroku
-    #return pymysql.connect(host='qvti2nukhfiig51b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',user='etiaob16vzbuo4cu',password='n8zzmlh5wda1ihbk',db='l5l3jax1eg8p2sg0')
-    return pymysql.connect(host='localhost',user='root',password='',db='jcp_helper_db')
+    #Conexion para railway
+
+    return pymysql.connect(host=os.environ["MYSQLHOST"],user=os.environ["MYSQLUSER"],password=os.environ["MYSQLPASSWORD"],db=os.environ["MYSQLDATABASE"])
+    #return pymysql.connect(host='localhost',user='root',password='',db='jcp_helper_db')
