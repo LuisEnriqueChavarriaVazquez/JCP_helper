c9540542b9ebe9cb7c1f06d1d9da62be9c684453
luis <luisechvz@gmail.com>
2023-01-10 08:59:31 -0600

    Conexión a su estado original

--- operacionesBD/conexion.py
+++ operacionesBD/conexion.py
@@ -6,5 +6,5 @@ def obtener_conexion():
 
     ##Amigos lo cambie temporalmente para hacer pruebas en mi PC
     
-    #return pymysql.connect(host=os.environ["MYSQLHOST"],user=os.environ["MYSQLUSER"],password=os.environ["MYSQLPASSWORD"],db=os.environ["MYSQLDATABASE"],port=7722)
-    return pymysql.connect(host='localhost',user='root',password='',db='jcp_helper_db')
+    return pymysql.connect(host=os.environ["MYSQLHOST"],user=os.environ["MYSQLUSER"],password=os.environ["MYSQLPASSWORD"],db=os.environ["MYSQLDATABASE"],port=7722)
+    #return pymysql.connect(host='localhost',user='root',password='',db='jcp_helper_db')
